80540984f05f20e1c6fc6529499adccc234b4273
zifengwang <zifengwang@bcm.cm.cluster>
2020-03-21 15:36:06 +0800

    add toy data

--- run_train.sh
+++ run_train.sh
@@ -1,4 +1,7 @@
-python -u main_findif.py train --data="FilterNYT" --use_gpu=True > train.log &
+#python -u main_findif.py train --data="FilterNYT" --use_gpu=True > train.log &
 # python -u main_findif.py train --data="NYT" --use_gpu=True > train.log &
 # python -u main_mil.py train --data="FilterNYT" --use_gpu=True > train.log &
 # python -u main_att.py train --data="FilterNYT" --use_gpu=True > train.log &
+srun --gres=gpu:1 python -u main_findif.py train --data="NYT" --use_gpu=True --num_epochs=2 > train.log & 
+# python -u main_findif.py train --data="NYT" --use_gpu=False --num_epochs=2 > train.log &
+
